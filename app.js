1662fda91cdbf76961c4d6e2b5a765a068f37bd1
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-04-06 12:15:48 -0700

    Making a grocery list item app and on hover it changes the style

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// TODO
+// Here we create a `GroceryList` component
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemhover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    let style = {
+      'font-weight': this.state.done ? 'bold' : ''
+    }
+    return (
+        <li style ={style} onMouseOver={this.onListItemhover.bind(this)}>{this.props.todo}</li>
+    );
+  }
+}
+
+let GroceryList = (props) => (
+ <ul>
+   {props.todos.map(todo =>
+      <GroceryListItem todo={todo}/>
+   )}
+ </ul>
+);
+
+let todos = ['Cucumbers', 'Kale', 'Tomatos'];
+ReactDOM.render(<GroceryList todos={todos}/>, document.getElementById("app"));
